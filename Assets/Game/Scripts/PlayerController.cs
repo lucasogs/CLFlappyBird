315d819bca0f782b517b7be696a220a6b39bcd95
Lucas Oliveira Gomes de Souza <73510928+lucasogs@users.noreply.github.com>
2021-11-24 11:07:43 -0300

    Movimentação
    - Movimentação Player

--- Assets/Game/Scripts/PlayerController.cs
+++ Assets/Game/Scripts/PlayerController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //Variables
+    [SerializeField] float upForce = 200f;
+    bool isDead = false;
+    Rigidbody2D rb2d;
+    //..
+
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+        IsDead();
     }
+
+    //Metodos
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(0, upForce));
+        }
+    }
+    void IsDead()
+    {
+        if (isDead == true)
+        {
+            Debug.Log("Player is dead");
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isDead = true;
+    }
+    //..
 }
